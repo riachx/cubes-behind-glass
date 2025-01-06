d3ac197562275a45c079d629580ab71ede7d305b
riachx <ria.fvista@gmail.com>
2025-01-06 01:39:41 -0800

    fixing comments

--- src/main.js
+++ src/main.js
@@ -1,134 +1,143 @@
 import * as THREE from 'three';
 import { MeshTransmissionMaterial } from '../MeshTransmissionMaterial';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { BokehPass } from 'three/addons/postprocessing/BokehPass.js';
-import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
-import { render } from '@react-three/fiber';
+import { RapierPhysics } from 'three/addons/physics/RapierPhysics.js';
+
+
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let modelStar;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
 
 
+// CORRECT COLORING
 renderer.toneMapping = THREE.ACESFilmicToneMapping; // Tone mapping for better color balance
-renderer.outputEncoding = THREE.sRGBEncoding; 
+renderer.outputEncoding = THREE.sRGBEncoding;
 
+// SHADOWS
+//renderer.shadowMap.enabled = true;
+//renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 
- 
-const controls = new OrbitControls( camera, renderer.domElement );
+// ROTATE CAMERA
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 window.addEventListener('resize', onWindowResize);
 
-
+// USER RESIZES WINDOW
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
 }
-
-renderer.setClearColor( 0xffffff, 0);
-
-const normalMapTexture = new THREE.TextureLoader().load('./assets/normal.png');
-//normalMapTexture.wrapS = new THREE.RepeatWrapping;
-//normalMapTexture.wrapT = new THREE.RepeatWrapping;
-//normalMapTexture.repeat.set(options.normalRepeat, options.normalRepeat);
-
-const geometry = new THREE.BoxGeometry( 4,4,0.1);
-const material = Object.assign(new MeshTransmissionMaterial(8), {
-    
-  /*clearcoatRoughness: 0,
-  transmission: 0.99,
-  chromaticAberration: 0.01,
-  //anisotropy: 0,
-  // Set to > 0 for diffuse roughness
-  roughness: 0.1,
-  thickness: 10,
-  ior: 1.2,
-  envMapIntensity: 50,
-  bloomstrength:0,
-  distortion: 0.5,
-  distortionScale: 20,*/
-  clearcoat: 1,
-    chromaticAberration: 0.0,
-    transmission: 0.95,
-    thickness: 0.5,
-    roughness: 0.0,
-    ior: 1.2,
-    //envMap: hdrEquirect,
-    //envMapIntensity: options.envMapIntensity,
-    //clearcoat: options.clearcoat,
-    //clearcoatRoughness: options.clearcoatRoughness,
-    
-    normalMap: normalMapTexture,
-    normalScale: new THREE.Vector2(4,4),
-    clearcoatNormalScale: 10,
-  
-
-});
-
-// normalMapTexture
-
-const cube = new THREE.Mesh( geometry, material );
-
-cube.position.z = 2;
-scene.add( cube );
-
-// Load a single texture
-const loader = new THREE.TextureLoader();
-const texture = loader.load('./assets/wood1.png');
-
-// Define the cube geometry
-const cube2_geo = new THREE.BoxGeometry(2, 2, 2);
-
-// Create a material with the texture applied to all sides
-const cube2_mat = new THREE.MeshStandardMaterial({ map: texture, 
-    roughness: 0.7, 
-    metalness: 0.1 
-});
-
-// Create the cube mesh and add it to the scene
-const cube2 = new THREE.Mesh(cube2_geo, cube2_mat);
-scene.add(cube2);
-
-
-const geometry2 = new THREE.SphereGeometry(1, 32, 32);
-const material2 = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
-const sphere = new THREE.Mesh( geometry2, material2 );
-sphere.position.x = 1
-scene.add( sphere );
 
 camera.position.z = 5;
 
+let physics;
+
+scene.background = new THREE.Color(0xffffff); 
+
+const geometry = new THREE.BoxGeometry(50, 9, 0.1);
+const material = Object.assign(new MeshTransmissionMaterial(8), {
+
+    clearcoatRoughness: 0,
+    transmission: 0.99,
+    chromaticAberration: 1.3,
+    roughness: 0.3,
+    thickness: 2,
+    ior: 1.2,
+    distortion: 0.3,
+    distortionScale: 0.1,
+
+});
+
+const wall = new THREE.Mesh(geometry, material);
+
+wall.userData.physics = { mass: 0 };
+wall.position.z = 2;
+scene.add(wall);
+
+
+
+
+physics = await RapierPhysics();
+
+
+
+const floor_geo = new THREE.BoxGeometry(200, 1, 200);
+const floor_mat = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const floor = new THREE.Mesh(floor_geo, floor_mat);
+
+floor.userData.physics = { mass: 0 };
+floor.position.y = -5;
+scene.add(floor);
+
+// LEFT WALL
+const wall1_geo = new THREE.BoxGeometry(1, 50, 100);
+const wall1 = new THREE.Mesh(wall1_geo, floor_mat);
+
+wall1.userData.physics = { mass: 0 };
+wall1.position.x = -20;
+scene.add(wall1);
+
+
+// BOXES 
+const boxes_geo = new THREE.BoxGeometry(2, 2, 2);
+const boxes_mat = new THREE.MeshPhysicalMaterial({
+
+});
+const boxes = new THREE.InstancedMesh(boxes_geo, boxes_mat, 500);
+boxes.instanceMatrix.setUsage(THREE.DynamicDrawUsage); // will be updated every frame
+boxes.userData.physics = { mass: 5, restitution: 0 };
+scene.add(boxes);
+
+const matrix = new THREE.Matrix4();
+
+const colors = [0xecf737, 0x28e110, 0x00fffc, 0xd859f8];
+
+for (let i = 0; i < boxes.count; i++) {
+
+    const x = Math.random() * 20 - 10;
+    const y = Math.random() * 200;
+    const z = Math.random() * 20 - 23;
+
+    matrix.setPosition(x, y, z);
+    boxes.setMatrixAt(i, matrix);
+    const randomColor = colors[Math.floor(Math.random() * 4)];
+    boxes.setColorAt(i, new THREE.Color(randomColor));
+}
+
+physics.addScene(scene);
+
 // LIGHTS
-const ambientLight = new THREE.AmbientLight(0xffe9db, 1);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
 scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffe9db, 10); 
-pointLight.position.set(2, 3, 4); 
-scene.add(pointLight);
+const pointLight = new THREE.PointLight(0x0012ff, 1);
+pointLight.position.set(-1, -2, 3);
+pointLight.castShadow = true;
 
-const pointLight2 = new THREE.PointLight(0xFFA500, 10); 
-pointLight2.position.set(-1, -5, 0); 
-scene.add(pointLight2);
 
-const directionalLight = new THREE.DirectionalLight(0xFFA500, 9); 
-directionalLight.position.set(1, 1, 0); 
-scene.add(directionalLight);
+const dirLight2 = new THREE.DirectionalLight(0xfff000, 5);
+dirLight2.position.set(2, -5, 1);
+scene.add(dirLight2);
 
+const dirLight = new THREE.DirectionalLight(0x28e110, 8);
+dirLight.position.set(1, 3, 0);
+scene.add(dirLight);
+
+const dirLight3 = new THREE.DirectionalLight(0x0090ff, 6);
+dirLight3.position.set(1, 10, 10);
+scene.add(dirLight3);
 
 
 function animate() {
 
-	cube2.rotation.x += 0.01;
-	cube2.rotation.y += 0.01;
-
-
-	renderer.render( scene, camera );
+    renderer.render(scene, camera);
 
 }
